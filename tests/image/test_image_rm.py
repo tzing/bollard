1c86140156993371fc280739e754fb01e3047fd4
Tzu-Ting <tzingshih@gmail.com>
2023-02-07 23:02:14 +0800

    add test

---
+++ tests/image/test_image_rm.py
@@ -0,0 +1,21 @@
+from unittest.mock import patch
+
+import pytest
+
+import bollard.image.rm as t
+
+
+def test_select_images():
+    with (
+        patch("bollard.image.data.get_image_ids"),
+        patch(
+            "bollard.image.data.get_image_data",
+            return_value=[
+                {"Id": "sha256:aaaa", "RepoTags": ["test:latest"]},
+                {"Id": "sha256:bbbb", "RepoTags": ["foo:latest"]},
+                {"Id": "sha256:cccc", "RepoTags": ["foo:2023.2.0"]},
+            ],
+        ),
+    ):
+        assert t.select_images(["aaaa"]) == ["sha256:aaaa"]
+        assert t.select_images([":latest"]) == ["sha256:aaaa", "sha256:bbbb"]
