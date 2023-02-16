c907af620c149aa5294689192168dbd3ce91c81d
Tzu-Ting <tzingshih@gmail.com>
2023-02-17 00:03:26 +0800

    add test

--- tests/image/test_image_rm.py
+++ tests/image/test_image_rm.py
@@ -17,3 +17,30 @@ def test_select_images():
     ):
         assert t.select_images(["aaaa"]) == ["sha256:aaaa"]
         assert t.select_images([":latest"]) == ["sha256:aaaa", "sha256:bbbb"]
+
+
+def test_interactive_select_image():
+    with (
+        patch("bollard.image.data.list_image_ids"),
+        patch(
+            "bollard.image.data.collect_fields",
+            return_value=[
+                {
+                    "id": "aaaa",
+                    "size": "10 kB",
+                    "created": "when",
+                    "repo_tag": "foo:2023.2.0",
+                },
+                {
+                    "id": "aaaa",
+                    "size": "10 kB",
+                    "created": "when",
+                    "repo_tag": "bar:latest",
+                },
+            ],
+        ),
+        patch(
+            "bollard.utils.interactive_select", return_value=["aaaa ...", "aaaa ..."]
+        ),
+    ):
+        assert t.interactive_select_image() == {"aaaa"}
